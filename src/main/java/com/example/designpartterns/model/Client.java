83752ffd6d012c0bb691c5558f10deec3b4619e7
edcorrea <edm.correa@hotmail.com>
2023-10-16 11:55:34 -0300

    feat: Model implementation - Client implementation

---
+++ src/main/java/com/example/designpartterns/model/Client.java
@@ -0,0 +1,34 @@
+package com.example.designpartterns.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Client {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	@ManyToOne
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
